d89a7aba8d724c485530e85d79674f3004694d6f
fsacado <loann.meignant@hotmail.fr>
2019-02-28 15:51:25 +0100

    First commit

---
+++ BillingBundle/BillingBundle.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: loann
+ * Date: 28/02/19
+ * Time: 15:27
+ */
+
+
+namespace Bluesquare\BillingBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BillingBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+
+//    public function getContainerExtension()
+//    {
+//        if (null === $this->extension)
+//            $this->extension = new StorageExtension();
+//        return $this->extension;
+//    }
+}
