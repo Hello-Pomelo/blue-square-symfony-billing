392beb2ae1f4f5204cfd3cb8fa1ed2ace874d4fa
cbeauvoi <cyprian@bluesquare.io>
2019-07-16 11:59:45 +0200

    setup namespace from Configuration

--- BillingBundle/DependencyInjection/Configuration.php
+++ BillingBundle/DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\SocialBundle\DependencyInjection;
+namespace Bluesquare\BillingBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
