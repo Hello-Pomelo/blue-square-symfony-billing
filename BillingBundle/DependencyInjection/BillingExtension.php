05ba73cd64451a0116f18ad7d3f5ab1871f1408b
cbeauvoi <cyprian@bluesquare.io>
2019-07-16 12:19:13 +0200

    dev push

--- BillingBundle/DependencyInjection/BillingExtension.php
+++ BillingBundle/DependencyInjection/BillingExtension.php
@@ -4,21 +4,18 @@ namespace Bluesquare\BillingBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
 class BillingExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-
-        $loader = new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__.'/../Resources/config')
-        );
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
         $loader->load('services.yaml');
+
+        $configuration = new Configuration();
 
         $this->addAnnotatedClassesToCompile([
             'Bluesquare\\BillingBundle\\Controller\\'
